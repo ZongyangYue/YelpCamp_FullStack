79aa0f645697799a513ef8eced989ecfc57d6bc1
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2022-10-24 16:54:19 -0700

    add delete function

--- app.js
+++ app.js
@@ -64,7 +64,11 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
-
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedCampground = await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 app.listen(3030, () => {
     console.log('Serving on port 3030!')
