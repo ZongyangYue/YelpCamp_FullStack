902453b1e7ea273f036d4649038e90ed9247ebbd
Zongyang Yue <zongyangyue@wifi-131-179-22-107.host.ucla.edu>
2022-10-25 14:04:43 -0700

    add images and image seeds

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, enim consequatur quae tenetur adipisci placeat perspiciatis reprehenderit esse rerum at? Quia quo, cumque cupiditate ipsum nemo consequatur nostrum quos libero?",
+            price
         })
         await camp.save();
     }
