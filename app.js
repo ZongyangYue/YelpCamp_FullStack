a7880f3aa99635fd7ddd11b920b715ea278ac4aa
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2023-01-03 23:02:25 -0800

    break out campground routes

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const { campgroundSchema, reviewSchema } = require('./schemas.js')
 const { validate } = require('./models/campground');
 const Review = require('./models/review');
 
+const campgrounds = require('./routes/campgrounds')
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 // , {
 //     useNewUrlParser: true,
@@ -59,44 +61,8 @@ const validateReview = (req, res, next) => {
     }
 }
 
+app.use('/campgrounds', campgrounds)
 
-
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-}))
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new')
-})
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show', { campground })
-}))
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground })
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const deletedCampground = await Campground.findByIdAndDelete(id)
-    res.redirect('/campgrounds')
-}))
 
 app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
