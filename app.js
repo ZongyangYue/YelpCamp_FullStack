8d4b22a8f3fe0e5421c5c83def7ee797f4d112b2
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2023-01-03 23:10:14 -0800

    break out review route

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const { validate } = require('./models/campground');
 const Review = require('./models/review');
 
 const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 // , {
@@ -62,23 +63,8 @@ const validateReview = (req, res, next) => {
 }
 
 app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
-
-app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(req.params.reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404));
