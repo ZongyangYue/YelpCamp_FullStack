ed125f67cce890c6cf344433110d0e3e07372b42
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2023-01-04 22:59:42 -0800

    useers controller

--- routes/users.js
+++ routes/users.js
@@ -4,47 +4,16 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
+const users = require('../models/users');
 
-router.post('/register', catchAsync(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        // this will hash and salt password
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash("Success", "Welcome to Yelp Camp!");
-            res.redirect('./campgrounds');
-        })
+router.get('/register', users.renderRegsiter)
 
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register')
-    }
+router.post('/register', catchAsync(users.register));
 
+router.get('/login', users.renderLogin);
 
-}));
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.get('/logout', users.logout)
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res) => {
-    req.logout(err => {
-        if (err) return next(err);
-        req.flash('success', "Goodbye!");
-        res.redirect('/campgrounds');
-    });
-
-})
 module.exports = router;
