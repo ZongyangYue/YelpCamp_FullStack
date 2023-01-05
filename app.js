cf874a55ac9521e0dcf1abfbe575852c0a3615c4
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2023-01-04 16:08:45 -0800

    register form and route

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 // , {
@@ -100,8 +101,10 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 
 app.all('*', (req, res, next) => {
