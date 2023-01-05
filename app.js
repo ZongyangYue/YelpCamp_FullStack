291c6e1f31a7e9aa9609229e36e03ba10bc368d0
Zongyang Yue <zongyangyue@Zongyangs-MacBook-Pro.local>
2023-01-04 19:32:51 -0800

    currentUser helper

--- app.js
+++ app.js
@@ -96,6 +96,7 @@ const validateReview = (req, res, next) => {
 
 //flash middleware before route handlers
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
