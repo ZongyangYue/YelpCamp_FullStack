38437119c20e9e2ce619414058e643740a77ced4
ZongyangYue <yuezongyang210@gmail.com>
2023-01-07 18:38:56 -0600

    fix bugs of upload to cloudinary

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,8 +2,8 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
-    api_key: process.env.ClOUDINARY_KEY,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 })
 
